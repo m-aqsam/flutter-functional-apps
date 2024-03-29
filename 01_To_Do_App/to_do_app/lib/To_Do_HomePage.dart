5e1488cbdf1bd9ce072e942bf397359aa0a35110
m_aqsam <maqsam1155@gmail.com>
2024-03-29 23:51:48 +0500

    To app page

---
+++ 01_To_Do_App/to_do_app/lib/To_Do_HomePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class To_Do_HomePage extends StatefulWidget {
+  const To_Do_HomePage({super.key});
+
+  @override
+  State<To_Do_HomePage> createState() => _To_Do_HomePageState();
+}
+
+class _To_Do_HomePageState extends State<To_Do_HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[300],
+    );
+  }
+}
