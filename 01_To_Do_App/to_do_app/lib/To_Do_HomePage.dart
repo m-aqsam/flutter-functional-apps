be4f41e2e03ef10d2875f178498ec3293d4a2fa2
m_aqsam <maqsam1155@gmail.com>
2024-04-01 23:28:15 +0500

    final update

--- 01_To_Do_App/to_do_app/lib/To_Do_HomePage.dart
+++ 01_To_Do_App/to_do_app/lib/To_Do_HomePage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:to_do_app/Setting_Page.dart';
 import 'package:to_do_app/components/MyListTile.dart';
 import 'package:to_do_app/components/My_DialogBox.dart';
 
@@ -18,8 +19,8 @@ class _To_Do_HomePageState extends State<To_Do_HomePage> {
   // To Do List
 
   List todoList = [
-    ["Do Exercise", false],
-    ["Buy Groceries", false],
+    ["Tap + to add new task ", false],
+    ["Swipe Left to Delete", false],
   ];
 
   // CheckBoxTap Method
@@ -69,17 +70,61 @@ class _To_Do_HomePageState extends State<To_Do_HomePage> {
     });
   }
 
+  // Edit Task Method
+
+  // My Method
+
+  // void editTask(index) {
+  //   showDialog(
+  //     context: context,
+  //     builder: (context) {
+  //       return My_DialogBox(
+  //         controller: _controller,
+  //         onSave: changeTask,
+  //         onCancel: cancelTask,
+  //       );
+  //     },
+  //   );
+  // }
+
+  // ChatGpt Method
+
+  void editTask(int index) {
+    _controller.text = todoList[index][0]; // Set the initial text in the dialog
+    showDialog(
+      context: context,
+      builder: (context) {
+        return My_DialogBox(
+          controller: _controller,
+          onSave: () => changeTask(index), // Pass the index argument here
+          onCancel: cancelTask,
+        );
+      },
+    );
+  }
+
+  // ChangeTask Method
+
+  void changeTask(index) {
+    setState(() {
+      //todoList.insert(index, [_controller.text, false]);
+      todoList[index][0] = _controller.text;
+      _controller.clear();
+      Navigator.of(context).pop();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[300],
+      backgroundColor: Colors.yellow[200],
 
       // App Bar
       appBar: AppBar(
         backgroundColor: Colors.yellow,
         title: Center(
           child: Text(
-            "TO DO",
+            "T O  D O",
             style: TextStyle(
               fontWeight: FontWeight.bold,
             ),
@@ -91,23 +136,75 @@ class _To_Do_HomePageState extends State<To_Do_HomePage> {
 
       floatingActionButton: FloatingActionButton(
         onPressed: createNewTask,
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.black,
         shape: CircleBorder(),
-        child: Icon(Icons.add),
+        child: Icon(
+          Icons.add,
+          color: Colors.yellow,
+          size: 30,
+        ),
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      // Bottom Nav Bar
+
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.yellow,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Icons Button
+
+            // Home
+
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.home,
+                size: 45,
+                color: Colors.black,
+              ),
+            ),
+
+            SizedBox(width: 140),
+
+            // Setting
+
+            IconButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (Context) {
+                    return Setting_Page();
+                  },
+                ));
+              },
+              icon: Icon(
+                Icons.settings,
+                size: 45,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
       ),
 
       // Body
 
-      body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          return MyListTile(
-            taskName: todoList[index][0],
-            taskCompleted: todoList[index][1],
-            onChanged: (value) => checkBoxTap(value, index),
-            deleteFunction: (context) => deleteTask(index),
-          );
-        },
+      body: Container(
+        height: 690,
+        child: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return MyListTile(
+              taskName: todoList[index][0],
+              taskCompleted: todoList[index][1],
+              onChanged: (value) => checkBoxTap(value, index),
+              deleteFunction: (context) => deleteTask(index),
+              editFunction: (context) => editTask(index),
+            );
+          },
+        ),
       ),
     );
   }
