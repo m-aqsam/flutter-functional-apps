83d464a61f6ec8e7c2a5ee1928d7260c65f6bb7a
m_aqsam <maqsam1155@gmail.com>
2024-05-27 16:16:44 +0500

    notes page

---
+++ 02_Notes_App/notes_app/lib/pages/Notes_Page.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Notes_Page extends StatefulWidget {
+  const Notes_Page({super.key});
+
+  @override
+  State<Notes_Page> createState() => _Notes_PageState();
+}
+
+class _Notes_PageState extends State<Notes_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // Continue
+
+        );
+  }
+}
