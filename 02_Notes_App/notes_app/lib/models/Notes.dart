4d173709f0d9d191b71e43d2b7a1396f31dab208
m_aqsam <maqsam1155@gmail.com>
2024-06-20 23:16:33 +0500

    update

--- 02_Notes_App/notes_app/lib/models/Notes.dart
+++ 02_Notes_App/notes_app/lib/models/Notes.dart
@@ -2,6 +2,8 @@ import 'package:isar/isar.dart';
 
 part 'note.g.dart'; // still not solve
 
+// I am leaving this project as it is and creating new project.
+
 @collection
 class Notes {
   Id id = Isar.autoIncrement;
