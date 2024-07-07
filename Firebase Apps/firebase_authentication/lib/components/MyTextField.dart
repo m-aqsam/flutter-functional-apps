4766a932f2fea416e6e79bde8c8391cac643a2ef
m_aqsam <maqsam1155@gmail.com>
2024-07-07 23:56:51 +0500

    textfield

---
+++ Firebase Apps/firebase_authentication/lib/components/MyTextField.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            hintText: hintText,
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            hintStyle: TextStyle(color: Colors.grey[500])),
+      ),
+    );
+  }
+}
