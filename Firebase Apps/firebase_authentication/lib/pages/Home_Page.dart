73df4f2fc603e5197c17815c4bf72ecdb94ecc77
m_aqsam <maqsam1155@gmail.com>
2024-07-08 03:11:11 +0500

    new changes

---
+++ Firebase Apps/firebase_authentication/lib/pages/Home_Page.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatefulWidget {
+  Home_Page({super.key});
+
+  @override
+  State<Home_Page> createState() => _Home_PageState();
+}
+
+class _Home_PageState extends State<Home_Page> {
+  void signUserOut() async {
+    FirebaseAuth.instance.signOut();
+  }
+
+  final user = FirebaseAuth.instance.currentUser!;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: signUserOut, icon: Icon(Icons.logout_rounded))
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "Logged in as : " + user.email!,
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
