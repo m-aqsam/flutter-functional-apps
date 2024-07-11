e1dfeb5b52e5eaa19d40a14038a7d08ad272ff44
m_aqsam <maqsam1155@gmail.com>
2024-07-11 07:51:00 +0500

    dark mode

---
+++ Firebase Apps/notes_app_online_database/lib/theme/Dark_Mode.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+    colorScheme: ColorScheme.dark(
+        surface: Colors.grey.shade900,
+        primary: Colors.grey.shade100, //button
+        secondary: Colors.grey.shade100, // tile
+        tertiary: Colors.black, //text
+        inversePrimary: Colors.grey.shade900 //icons
+        ));
