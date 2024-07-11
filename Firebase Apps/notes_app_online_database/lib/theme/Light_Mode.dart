0e2dbd064d5f615550f56e835d8008c4846b0c68
m_aqsam <maqsam1155@gmail.com>
2024-07-11 07:51:05 +0500

    light mode

---
+++ Firebase Apps/notes_app_online_database/lib/theme/Light_Mode.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+    colorScheme: ColorScheme.light(
+        surface: Colors.grey.shade300,
+        primary: Colors.grey.shade900,
+        secondary: Colors.grey.shade100, //tile
+        tertiary: Colors.black,
+        inversePrimary: Colors.grey.shade900 //icons
+        ));
