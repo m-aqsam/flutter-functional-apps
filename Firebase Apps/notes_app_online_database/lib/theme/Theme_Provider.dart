e980bc6236d78d193198f8de930e61ac3675671a
m_aqsam <maqsam1155@gmail.com>
2024-07-11 07:51:10 +0500

    theme

---
+++ Firebase Apps/notes_app_online_database/lib/theme/Theme_Provider.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_final_fields, unused_field
+
+import 'package:flutter/material.dart';
+import 'package:notes_app_online_database/theme/Dark_Mode.dart';
+import 'package:notes_app_online_database/theme/Light_Mode.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  // Initialize light mode
+  ThemeData _themeData = lightMode;
+
+  // get current theme
+
+  ThemeData get themeData => _themeData;
+
+  // if current theme dark mode
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  // set theme
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  // toggle theme
+
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
