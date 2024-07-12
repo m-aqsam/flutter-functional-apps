121cd18f509e4eeaf21b889c3f983d172c291f63
m_aqsam <maqsam1155@gmail.com>
2024-07-12 23:33:50 +0500

    auth service

---
+++ Firebase Apps/chat_application/lib/auth/Auth_Service.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  // instance of auth
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in
+
+  Future<UserCredential> signInWithEmailPassword(String email, password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+  // sign out
+
+  Future<void> signOut() async {
+    return await _auth.signOut();
+  }
+
+  // Sign Up
+
+  Future<UserCredential> signUpWithEmailPassword(String email, password) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+}
