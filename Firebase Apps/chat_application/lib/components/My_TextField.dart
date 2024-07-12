596a32a92e63357c27ac4613e67b5fdcb085bf06
m_aqsam <maqsam1155@gmail.com>
2024-07-12 23:34:31 +0500

    textfield com

---
+++ Firebase Apps/chat_application/lib/components/My_TextField.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class My_TextField extends StatelessWidget {
+  final String hintText;
+  final bool obsecureText;
+  final TextEditingController controller;
+  My_TextField({
+    super.key,
+    required this.hintText,
+    required this.obsecureText,
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: TextField(
+        obscureText: obsecureText,
+        controller: controller,
+        decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.tertiary,
+            )),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.primary,
+            )),
+            fillColor: Theme.of(context).colorScheme.secondary,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary)),
+      ),
+    );
+  }
+}
