921be933c6739b181fa5d47d2293dc209d53cd68
m_aqsam <maqsam1155@gmail.com>
2024-07-12 23:30:23 +0500

    latest update

---
+++ Firebase Apps/chat_application/lib/main.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:chat_application/auth/Auth_Gate.dart';
+import 'package:chat_application/firebase_options.dart';
+import 'package:chat_application/theme/Light_Mode.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthGate(),
+      theme: lightMode,
+    );
+  }
+}
