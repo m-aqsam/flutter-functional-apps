fdc821315dda54f85e60e4981326131e431436d2
m_aqsam <maqsam1155@gmail.com>
2024-07-13 22:30:35 +0500

    update drawer

--- Firebase Apps/chat_application/lib/components/My_Drawer.dart
+++ Firebase Apps/chat_application/lib/components/My_Drawer.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:chat_application/auth/Auth_Service.dart';
+import 'package:chat_application/services/auth/Auth_Service.dart';
 import 'package:chat_application/pages/Settings_Page.dart';
 import 'package:flutter/material.dart';
 
