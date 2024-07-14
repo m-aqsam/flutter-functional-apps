3b439b9a1bf679f1ef90693b7a5f337f8a15a926
m_aqsam <maqsam1155@gmail.com>
2024-07-15 03:29:35 +0500

    add dark mode

--- Firebase Apps/chat_application/lib/theme/Dark_Mode.dart
+++ Firebase Apps/chat_application/lib/theme/Dark_Mode.dart
@@ -3,5 +3,9 @@ import 'package:flutter/material.dart';
 ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
     surface: Colors.grey.shade900,
+    primary: Colors.grey.shade600,
+    secondary: Colors.grey.shade700,
+    tertiary: Colors.grey.shade800,
+    inversePrimary: Colors.grey.shade300,
   ),
 );
