a457cf9480b540131b1290ae08636e7132ce8fa1
m_aqsam <maqsam1155@gmail.com>
2024-07-15 03:29:41 +0500

    theme provider

---
+++ Firebase Apps/chat_application/lib/theme/Theme_Provider.dart
@@ -0,0 +1,25 @@
+import 'package:chat_application/theme/Dark_Mode.dart';
+import 'package:chat_application/theme/Light_Mode.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightMode;
+    }
+  }
+}
