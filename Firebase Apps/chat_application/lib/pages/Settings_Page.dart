52f2bfa4f57ac1525765a1083a37b1e9a3378ed7
m_aqsam <maqsam1155@gmail.com>
2024-07-15 03:29:23 +0500

    setting page

--- Firebase Apps/chat_application/lib/pages/Settings_Page.dart
+++ Firebase Apps/chat_application/lib/pages/Settings_Page.dart
@@ -1,6 +1,9 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:chat_application/theme/Theme_Provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Settings_Page extends StatelessWidget {
   const Settings_Page({super.key});
@@ -8,12 +11,39 @@ class Settings_Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Settings"),
-      ),
-      body: Center(
-        child: Text("Settings Page"),
-      ),
-    );
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        appBar: AppBar(
+          title: Text("Settings"),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Column(
+          children: [
+            // Dark or light mode
+
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: EdgeInsets.all(16),
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Dark Mode"),
+                  CupertinoSwitch(
+                    value: Provider.of<ThemeProvider>(context, listen: false)
+                        .isDarkMode,
+                    onChanged: (value) {
+                      Provider.of<ThemeProvider>(context, listen: false)
+                          .toggleTheme();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
