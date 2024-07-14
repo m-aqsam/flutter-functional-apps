a8cfffc79600062518c506df4df0fb044fd45dcc
m_aqsam <maqsam1155@gmail.com>
2024-07-15 03:28:53 +0500

    updated

--- Firebase Apps/chat_application/lib/components/User_Tile.dart
+++ Firebase Apps/chat_application/lib/components/User_Tile.dart
@@ -21,7 +21,7 @@ class User_Tile extends StatelessWidget {
           color: Theme.of(context).colorScheme.secondary,
           borderRadius: BorderRadius.circular(12),
         ),
-        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 25),
         padding: EdgeInsets.all(20),
         child: Row(
           children: [
