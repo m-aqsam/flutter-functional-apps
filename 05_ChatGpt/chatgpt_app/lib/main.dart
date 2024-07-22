66af2ed373eec60c95c587779c79005e6d3b7a75
m_aqsam <maqsam1155@gmail.com>
2024-07-23 01:15:46 +0500

    update

---
+++ 05_ChatGpt/chatgpt_app/lib/main.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:chatgpt_app/pages/Chat_Page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Chat_Page(),
+    );
+  }
+}
