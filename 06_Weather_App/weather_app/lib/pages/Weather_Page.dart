264c036c41cf7b7e4e2e71f9b9b419ad3b783151
m_aqsam <maqsam1155@gmail.com>
2024-07-24 04:26:15 +0500

    weather app

---
+++ 06_Weather_App/weather_app/lib/pages/Weather_Page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Weather_Page extends StatefulWidget {
+  const Weather_Page({super.key});
+
+  @override
+  State<Weather_Page> createState() => _Weather_PageState();
+}
+
+class _Weather_PageState extends State<Weather_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
