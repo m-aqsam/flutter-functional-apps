8d14cd567ae504c0d6408bd84b0cb5748681daef
m_aqsam <maqsam1155@gmail.com>
2024-08-03 13:42:09 +0500

    update

--- 07_Counter_App/counter_app/lib/models/Counter_Model.dart
+++ 07_Counter_App/counter_app/lib/models/Counter_Model.dart
@@ -8,3 +8,14 @@ class CounterProvider extends ChangeNotifier {
     notifyListeners();
   }
 }
+
+// ****************** Provider Guide ********************
+
+/* 
+
+1: Add Provider Package => (flutter pub add provider)
+2: Define/ Return Provider in Main.dart as (Multiple / Single Provider)
+3: Use the class in the widget tree using Consumer widget
+  => (Wrap the Scaffold with Consumer Package Return Builder )
+
+*/
