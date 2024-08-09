00540b4b0afd5b6800048ee0c58bb6a2945f7f8b
m_aqsam <maqsam1155@gmail.com>
2024-08-09 23:22:42 +0500

    update

---
+++ State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Cart/ui/Cart_Page.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Cart_Page extends StatefulWidget {
+  const Cart_Page({super.key});
+
+  @override
+  State<Cart_Page> createState() => _Cart_PageState();
+}
+
+class _Cart_PageState extends State<Cart_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: Text("Cart"),
+        backgroundColor: Colors.yellow,
+      ),
+    );
+  }
+}
