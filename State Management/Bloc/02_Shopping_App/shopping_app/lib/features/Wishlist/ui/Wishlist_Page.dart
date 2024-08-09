9cd7212ced044801fb985bf2bd487a9e0a4679a6
m_aqsam <maqsam1155@gmail.com>
2024-08-09 23:22:59 +0500

    update

---
+++ State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Wishlist/ui/Wishlist_Page.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Wishlist_Page extends StatefulWidget {
+  const Wishlist_Page({super.key});
+
+  @override
+  State<Wishlist_Page> createState() => _Wishlist_PageState();
+}
+
+class _Wishlist_PageState extends State<Wishlist_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: Text("WishList"),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
