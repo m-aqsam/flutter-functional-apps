fe25809bdf90139de2419f834ed847fa6be05512
m_aqsam <maqsam1155@gmail.com>
2024-08-14 00:01:46 +0500

    update

--- State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Home/Bloc/bloc/home_event.dart
+++ State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Home/Bloc/bloc/home_event.dart
@@ -3,6 +3,8 @@ part of 'home_bloc.dart';
 @immutable
 sealed class HomeEvent {}
 
+class HomeInitialEvent extends HomeEvent {}
+
 class HomeProductWishlistButtonClickedEvent extends HomeEvent {}
 
 class HomeProductCartButtonClickedEvent extends HomeEvent {}
