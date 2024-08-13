b50ae71de638234a7e38b497be15b8784d4c4b2e
m_aqsam <maqsam1155@gmail.com>
2024-08-14 00:01:36 +0500

    update

---
+++ State Management/Bloc/02_Shopping_App/shopping_app/lib/Data/Grocery_Data.dart
@@ -0,0 +1,36 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    // map1
+
+    {
+      "id": 1,
+      "name": "Banana",
+      'description': 'Fresh Banana from Ecuadoe',
+      "price": 1.99,
+      'imageUrl':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXK-Kr7GuztnmiaByNnTh0DM0qS4XkqIdIWSGk6q4XuNP6mUO0_WvjExqDLn47mhwneYE&usqp=CAU',
+    },
+
+    // map2
+
+    {
+      "id": 2,
+      "name": "Apple",
+      'description': 'Organic,locally grained apples',
+      "price": 0.99,
+      'imageUrl':
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrMxK6l9zUnNiuGbK6uQIXtzWC6_4XuICfZQ&s',
+    },
+
+    // map3
+
+    {
+      "id": 3,
+      "name": "Bread",
+      'description': 'Fresh bread-milky bread',
+      "price": 1.49,
+      'imageUrl':
+          'https://assets.bonappetit.com/photos/5c62e4a3e81bbf522a9579ce/16:9/w_4000,h_2250,c_limit/milk-bread.jpg'
+    }
+  ];
+}
