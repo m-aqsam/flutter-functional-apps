821fb0d9eae065cdbd1e668885a45420ef7d0978
m_aqsam <maqsam1155@gmail.com>
2024-08-14 00:01:41 +0500

    update

--- State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Home/Bloc/bloc/home_bloc.dart
+++ State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Home/Bloc/bloc/home_bloc.dart
@@ -2,18 +2,41 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:shopping_app/Data/Grocery_Data.dart';
+import 'package:shopping_app/features/Home/Models/Grocery_Model.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
+    on<HomeInitialEvent>(homeInitialEvent);
     on<HomeCartNavigateButtonClickedEvent>(homeCartNavigateButtonClickedEvent);
     on<HomeWishlistNavigateButtonClickedEvent>(
         homeWishlistNavigateButtonClickedEvent);
     on<HomeProductCartButtonClickedEvent>(homeProductCartButtonClickedEvent);
     on<HomeProductWishlistButtonClickedEvent>(
         homeProductWishlistButtonClickedEvent);
+  }
+
+  FutureOr<void> homeInitialEvent(
+      HomeInitialEvent event, Emitter<HomeState> emit) async {
+    emit(HomeLoadingState());
+    await Future.delayed(Duration(seconds: 3));
+
+    emit(HomeLoadingSuccessState(
+      products: GroceryData.groceryProducts
+          .map(
+            (e) => ProductDataModel(
+              id: e['id'],
+              name: e['name'],
+              description: e['description'],
+              price: e['price'],
+              imageUrl: e['imageUrl'],
+            ),
+          )
+          .toList(),
+    ));
   }
 
   FutureOr<void> homeCartNavigateButtonClickedEvent(
