d5a986bf276914a1c2aa3e35dbc63260242b1aed
m_aqsam <maqsam1155@gmail.com>
2024-08-21 23:27:13 +0500

    updated

--- State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Cart/ui/Cart_Page.dart
+++ State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Cart/ui/Cart_Page.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopping_app/features/Cart/Bloc/bloc/cart_bloc.dart';
+import 'package:shopping_app/features/Cart/ui/Cart_Tile_Widget.dart';
 
 class Cart_Page extends StatefulWidget {
   const Cart_Page({super.key});
@@ -10,13 +13,43 @@ class Cart_Page extends StatefulWidget {
 }
 
 class _Cart_PageState extends State<Cart_Page> {
+  final CartBloc cartBloc = CartBloc();
+  @override
+  void initState() {
+    cartBloc.add(CartInitialEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Cart"),
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.teal,
+      ),
+      body: BlocConsumer<CartBloc, CartState>(
+        bloc: cartBloc,
+        listenWhen: (previous, current) => current is CartActionState,
+        buildWhen: (previous, current) => current is! CartActionState,
+        listener: (context, state) {},
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case CartSuccessState:
+              final cartSuccess = state as CartSuccessState;
+
+              return ListView.builder(
+                itemCount: cartSuccess.cartItems.length,
+                itemBuilder: (context, index) => CartTileWidget(
+                  productDataModel: cartSuccess.cartItems[index],
+                  cartBloc: cartBloc,
+                ),
+              );
+
+            default:
+          }
+          return Container();
+        },
       ),
     );
   }
