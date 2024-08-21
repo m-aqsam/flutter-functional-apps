5d033f36d8c8741df3137aae47fa9ef2ad6c1761
m_aqsam <maqsam1155@gmail.com>
2024-08-21 23:28:14 +0500

    new update

---
+++ State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Wishlist/ui/Wishlist_Tile_Widget.dart
@@ -0,0 +1,100 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:shopping_app/features/Cart/Bloc/bloc/cart_bloc.dart';
+import 'package:shopping_app/features/Home/Bloc/bloc/home_bloc.dart';
+
+import 'package:shopping_app/features/Home/Models/Grocery_Model.dart';
+import 'package:shopping_app/features/Wishlist/Bloc/bloc/wishlist_bloc.dart';
+
+class WishlistTileWidget extends StatelessWidget {
+  final ProductDataModel productDataModel;
+  final WishlistBloc wishlistBloc;
+  WishlistTileWidget({
+    super.key,
+    required this.productDataModel,
+    required this.wishlistBloc,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Image
+
+          Container(
+            height: 200,
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(productDataModel.imageUrl),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+
+          // name
+
+          Text(
+            productDataModel.name,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          // description
+          Text(productDataModel.description),
+
+          // Price
+
+          SizedBox(height: 10),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '\$ ' + productDataModel.price.toString(),
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              // Buttons
+
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      // homeBloc.add(HomeProductWishlistButtonClickedEvent());
+                    },
+                    icon: Icon(Icons.favorite_border),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // homeBloc.add(HomeProductCartButtonClickedEvent(
+                      //   clickedProduct: productDataModel,
+                      // ));
+                    },
+                    icon: Icon(Icons.shopping_bag_outlined),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
