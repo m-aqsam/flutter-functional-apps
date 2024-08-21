2ccf04624d0f77046432a650e7a7563de8a7da56
m_aqsam <maqsam1155@gmail.com>
2024-08-21 23:28:02 +0500

    new update

---
+++ State Management/Bloc/02_Shopping_App/shopping_app/lib/features/Wishlist/Bloc/bloc/wishlist_state.dart
@@ -0,0 +1,14 @@
+part of 'wishlist_bloc.dart';
+
+@immutable
+sealed class WishlistState {}
+
+sealed class WishlistActionState extends WishlistState {}
+
+class WishlistInitial extends WishlistState {}
+
+class WishlistSuccessState extends WishlistState {
+  final List<ProductDataModel> wishlistItems;
+
+  WishlistSuccessState({required this.wishlistItems});
+}
