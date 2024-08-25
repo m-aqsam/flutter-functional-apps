ac74150eb417f34dc4e47569cb8ddc3bd4c095f7
m_aqsam <maqsam1155@gmail.com>
2024-08-25 23:58:26 +0500

    update

---
+++ 06_Weather_App/weather_app_new/lib/Weather_Page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Weather_Page extends StatelessWidget {
+  const Weather_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+    );
+  }
+}
