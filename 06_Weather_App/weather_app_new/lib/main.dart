db48fef38ed1b34d4f76c28b9311e34062a2870e
m_aqsam <maqsam1155@gmail.com>
2024-08-26 23:57:42 +0500

    update

--- 06_Weather_App/weather_app_new/lib/main.dart
+++ 06_Weather_App/weather_app_new/lib/main.dart
@@ -15,9 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Weather_Page(
-          //
-          ),
+      home: Weather_Page(),
     );
   }
 }
