4e185697be0ff689e24e5985393313237ddbed03
m_aqsam <maqsam1155@gmail.com>
2024-08-26 23:58:30 +0500

    new changes

--- 06_Weather_App/weather_app_new/lib/Weather_Page.dart
+++ 06_Weather_App/weather_app_new/lib/Weather_Page.dart
@@ -8,7 +8,7 @@ class Weather_Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      backgroundColor: Colors.pink,
     );
   }
 }
