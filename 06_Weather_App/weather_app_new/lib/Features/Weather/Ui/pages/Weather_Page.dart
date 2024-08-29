a27394601138c77d615b7d0a1543abefdd86ca59
m_aqsam <maqsam1155@gmail.com>
2024-08-29 05:13:33 +0500

    updated

--- 06_Weather_App/weather_app_new/lib/Features/Weather/Ui/pages/Weather_Page.dart
+++ 06_Weather_App/weather_app_new/lib/Features/Weather/Ui/pages/Weather_Page.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -8,7 +10,288 @@ class Weather_Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink,
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment(3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment(-3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment(0, -1.2),
+                child: Container(
+                  height: 300,
+                  width: 600,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFFAB40),
+                  ),
+                ),
+              ),
+
+              // Backdrop Filter
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                ),
+              ),
+
+              // Main Column
+
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Location
+
+                    Text(
+                      "📍 Bahawalpur",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+
+                    // Greeting
+
+                    Text(
+                      "Good Morning",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    // image
+                    Image.asset("lib/Assets/rainy.png"),
+                    SizedBox(height: 10),
+                    // Temp Text
+                    Center(
+                        child: Text(
+                      "21°C",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 55,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )),
+
+                    // Weather Text
+
+                    Center(
+                        child: Text(
+                      "THUNDERSTORM",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    )),
+                    SizedBox(height: 5),
+
+                    // Date & Time
+
+                    Center(
+                        child: Text(
+                      "Friday 16 . 09:41am",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    )),
+                    SizedBox(height: 30),
+
+                    // Row (Sunset and sunrise )
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // Sunrise
+
+                        Row(
+                          children: [
+                            Image.asset(
+                              "lib/Assets/clear sun.png",
+                              scale: 16,
+                            ),
+                            SizedBox(width: 3),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Sunrise",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                                SizedBox(height: 3),
+                                Text(
+                                  "5:44 am",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+
+                        // Sunset
+
+                        Row(
+                          children: [
+                            Image.asset(
+                              "lib/Assets/clear_moon.png",
+                              scale: 16,
+                            ),
+                            SizedBox(width: 3),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Sunset",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                                SizedBox(height: 3),
+                                Text(
+                                  "5:34 pm",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+
+                    // Divider
+
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        child: Divider(
+                          thickness: 1,
+                          color: Colors.white.withOpacity(0.5),
+                        )),
+
+                    // Row (High $ Low Temp)
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // High Temp
+
+                        Row(
+                          children: [
+                            Image.asset(
+                              "lib/Assets/hot temp.png",
+                              scale: 100,
+                            ),
+                            SizedBox(width: 3),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Max Temp",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                                SizedBox(height: 3),
+                                Text(
+                                  "32°C",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+
+                        // Cold Temp
+
+                        Row(
+                          children: [
+                            Image.asset(
+                              "lib/Assets/cold temp.png",
+                              scale: 100,
+                            ),
+                            SizedBox(width: 3),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Temp Min",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                                SizedBox(height: 3),
+                                Text(
+                                  "18°C",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
