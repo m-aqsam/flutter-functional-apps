365ea7b7288201326fd15d3c3c077483ce59c4ec
m_aqsam <maqsam1155@gmail.com>
2024-08-31 23:42:42 +0500

    New changes

--- 06_Weather_App/weather_app_new/lib/Features/Weather/Bloc/bloc/weather_bloc.dart
+++ 06_Weather_App/weather_app_new/lib/Features/Weather/Bloc/bloc/weather_bloc.dart
@@ -4,34 +4,30 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather/weather.dart';
-import 'package:weather_app_new/Features/Weather/Data/Weather_Data.dart';
+import 'package:weather_app_new/Features/Weather/Data/Weather_Data.dart'; // Import the weather package
 
 part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   WeatherBloc() : super(WeatherInitial()) {
-    print("WeatherBloc initialized");
-    on<WeatherFetchEvent>((event, emit) async {
-      emit(WeatherLoadingState());
-      try {
-        print("WeatherFetchEvent received: ${event.position}");
+    on<WeatherFetchEvent>(weatherFetchEvent);
+  }
 
-        WeatherFactory wf = WeatherFactory('b7359c63c28d04a3562a5d0bc0ec7113',
-            language: Language.ENGLISH);
-        print("WeatherFactory created");
+  FutureOr<void> weatherFetchEvent(
+      WeatherFetchEvent event, Emitter<WeatherState> emit) async {
+    try {
+      WeatherFactory wf = WeatherFactory(API_KEY, language: Language.ENGLISH);
+      print("WeatherFactory created");
 
-        Weather weather = await wf.currentWeatherByLocation(
-            event.position.latitude, event.position.longitude);
+      Weather weather = await wf.currentWeatherByCityName("London");
 
-        print("Weather data fetched: $weather");
+      print("Weather data fetched: $weather");
 
-        emit(WeatherSuccessState(weather));
-      } catch (e, stackTrace) {
-        print("Error fetching weather: $e");
-        print("Stack trace: $stackTrace");
-        emit(WeatherErrorState());
-      }
-    });
+      emit(WeatherSuccessState(weather));
+    } catch (e) {
+      print("Error fetching weather: $e");
+      emit(WeatherErrorState());
+    }
   }
 }
