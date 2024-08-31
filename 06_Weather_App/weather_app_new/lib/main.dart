3620378b4c3c02763aeac033a088264c667d0358
m_aqsam <maqsam1155@gmail.com>
2024-08-31 23:42:31 +0500

    Update

--- 06_Weather_App/weather_app_new/lib/main.dart
+++ 06_Weather_App/weather_app_new/lib/main.dart
@@ -17,64 +17,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FutureBuilder(
-        future: _determinePosition(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            print("Position obtained: ${snapshot.data}");
-            final position = snapshot.data as Position;
-            final weatherBloc = WeatherBloc();
-
-            weatherBloc.add(WeatherFetchEvent(position));
-
-            return BlocProvider<WeatherBloc>(
-              create: (context) => weatherBloc,
-              child: Weather_Page(),
-            );
-          } else if (snapshot.hasError) {
-            print("Error obtaining position: ${snapshot.error}");
-            return Scaffold(
-              body: Center(
-                child: Text("Error: ${snapshot.error}"),
-              ),
-            );
-          } else {
-            print("Waiting for position...");
-            return Scaffold(
-              body: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-        },
+      home: BlocProvider(
+        create: (_) => WeatherBloc(),
+        child: Weather_Page(),
       ),
     );
   }
-}
-
-Future<Position> _determinePosition() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    return Future.error('Location services are disabled.');
-  }
-
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      return Future.error('Location permissions are denied');
-    }
-  }
-
-  if (permission == LocationPermission.deniedForever) {
-    return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
-  }
-
-  print("Fetching current position...");
-
-  return await Geolocator.getCurrentPosition();
 }
