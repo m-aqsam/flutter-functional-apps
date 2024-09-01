7b411a377abbd784660db425c6606d1428aef6b7
m_aqsam <maqsam1155@gmail.com>
2024-09-01 23:56:36 +0500

    new changes

---
+++ 07_Ecommerce_App/e_shop/lib/main.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
