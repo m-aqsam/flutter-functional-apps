74f0a50dfed4b2edbcbb066e4a0bf158c71287b7
m_aqsam <maqsam1155@gmail.com>
2024-09-01 23:59:33 +0500

    text field theme

--- 07_Ecommerce_App/e_shop/lib/utlis/theme/custom_themes/text_field_theme.dart
+++ 07_Ecommerce_App/e_shop/lib/utlis/theme/custom_themes/text_field_theme.dart
@@ -32,5 +32,36 @@ class ETextFormFieldTheme {
       borderRadius: BorderRadius.circular(14),
       borderSide: const BorderSide(width: 2, color: Colors.orange),
     ),
-  ); // Input Decoration Theme
+  );
+
+  static InputDecorationTheme darkInputDecorationTheme = InputDecorationTheme(
+    errorMaxLines: 2,
+    prefixIconColor: Colors.grey,
+    suffixIconColor: Colors.grey,
+// constraints: const BoxConstraints.expand (height: 14.inputFieldHeight),
+    labelStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.white),
+    hintStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.white),
+    floatingLabelStyle:
+        const TextStyle().copyWith(color: Colors.white.withOpacity(0.8)),
+    border: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+    enabledBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.grey),
+    ),
+    focusedBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.white),
+    ),
+    errorBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1, color: Colors.red),
+    ),
+    focusedErrorBorder: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 2, color: Colors.orange),
+    ),
+  ); // InputDecoration Theme // Input Decoration Theme
 }
