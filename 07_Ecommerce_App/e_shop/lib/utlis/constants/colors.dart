87d536ab31dce6151f94ba3be14a29489121b62a
m_aqsam <maqsam1155@gmail.com>
2024-09-02 23:59:20 +0500

    Update

---
+++ 07_Ecommerce_App/e_shop/lib/utlis/constants/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EColors {
+  EColors._();
+
+  // App Basic Colors
+  static const Color primary = Color(0xFF4b68ff);
+  static const Color secondary = Color(0xFFFFE24B);
+  static const Color accent = Color(0xFFb0c7ff);
+// Text Colors
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color textSecondary = Color(0xFF6C757D);
+  static const Color textWhite = Colors.white;
+// Background Colors
+  static const Color Light = Color(0xFFF6F6F6);
+  static const Color dark = Color(0xFF272727);
+  static const Color primaryBackground = Color(0xFFF3F5FF);
+}
