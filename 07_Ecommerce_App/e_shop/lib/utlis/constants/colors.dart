a771ee1fc8d9939080b67b3b6b31b19b43a800a8
m_aqsam <maqsam1155@gmail.com>
2024-09-02 23:59:34 +0500

    Update

--- 07_Ecommerce_App/e_shop/lib/utlis/constants/colors.dart
+++ 07_Ecommerce_App/e_shop/lib/utlis/constants/colors.dart
@@ -15,4 +15,5 @@ class EColors {
   static const Color Light = Color(0xFFF6F6F6);
   static const Color dark = Color(0xFF272727);
   static const Color primaryBackground = Color(0xFFF3F5FF);
+  //
 }
