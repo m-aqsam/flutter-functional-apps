6a24fcc3bcf7d5c5bdd368307869edad29ed0837
m_aqsam <maqsam1155@gmail.com>
2024-09-02 23:59:26 +0500

    Update

--- 07_Ecommerce_App/e_shop/lib/utlis/theme/theme.dart
+++ 07_Ecommerce_App/e_shop/lib/utlis/theme/theme.dart
@@ -1,5 +1,10 @@
 import 'package:e_shop/utlis/theme/custom_themes/appbar_theme.dart';
+import 'package:e_shop/utlis/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:e_shop/utlis/theme/custom_themes/checkbox_theme.dart';
+import 'package:e_shop/utlis/theme/custom_themes/chip_theme.dart';
 import 'package:e_shop/utlis/theme/custom_themes/elevated_button_theme.dart';
+import 'package:e_shop/utlis/theme/custom_themes/outlined_button_theme.dart';
+import 'package:e_shop/utlis/theme/custom_themes/text_field_theme.dart';
 import 'package:e_shop/utlis/theme/custom_themes/text_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,6 +21,11 @@ class EShopTheme {
     textTheme: ETextTheme.lightTextTheme,
     elevatedButtonTheme: EElevatedButtonTheme.lightElevatedButtonTheme,
     appBarTheme: EAppBarTheme.lightAppBarTheme,
+    inputDecorationTheme: ETextFormFieldTheme.lightInputDecorationTheme,
+    outlinedButtonTheme: EOutlinedButtonTheme.lightOutlinedButtonTheme,
+    chipTheme: EChipTheme.lightChipTheme,
+    checkboxTheme: ECheckboxTheme.lightCheckboxTheme,
+    bottomSheetTheme: EBottomSheetTheme.lightBottomSheetTheme,
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
@@ -26,5 +36,10 @@ class EShopTheme {
     textTheme: ETextTheme.darkTextTheme,
     elevatedButtonTheme: EElevatedButtonTheme.darkElevatedButtonTheme,
     appBarTheme: EAppBarTheme.darkAppBarTheme,
+    inputDecorationTheme: ETextFormFieldTheme.darkInputDecorationTheme,
+    outlinedButtonTheme: EOutlinedButtonTheme.darkOutlinedButtonTheme,
+    chipTheme: EChipTheme.darkChipTheme,
+    checkboxTheme: ECheckboxTheme.darkCheckboxTheme,
+    bottomSheetTheme: EBottomSheetTheme.darkBottomSheetTheme,
   );
 }
