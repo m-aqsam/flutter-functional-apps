4507462d7b5eae1a41c3521f6295ce9241b920b1
m_aqsam <maqsam1155@gmail.com>
2024-09-06 23:40:36 +0500

    New updates

---
+++ 07_Ecommerce_App/e_shop/lib/utlis/formatters/formatters.dart
@@ -0,0 +1,26 @@
+import 'package:intl/intl.dart';
+
+class EFormastters {
+  static String formatDate(DateTime? date) {
+    date ??= DateTime.now();
+    {
+      return DateFormat('dd-MMM-yyyy ').format(date);
+    } // Customize the date format as needed
+  }
+
+  static String formatCurrency(double amount) {
+    return NumberFormat.currency(locale: 'en_US', symbol: '\$').format(amount);
+  }
+
+  static String formatPhoneNumber(String phoneNumber) {
+    // Assuming a 10-digit US phone number format: (123) 456-7890
+    if (phoneNumber.length == 10) {
+      return ' (${phoneNumber.substring(0, 3)}) ${phoneNumber.substring(3, 6)} ${phoneNumber.substring(6)}';
+    } else if (phoneNumber.length == 11) {
+      return ' (${phoneNumber.substring(0, 4)}) ${phoneNumber.substring(4, 7)} ${phoneNumber.substring(7)}';
+    }
+
+    // Add more conditions if required according to phone formats
+    return phoneNumber;
+  }
+}
