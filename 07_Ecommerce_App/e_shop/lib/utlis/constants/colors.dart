1cf5453fe857f5bfdd3e155a637da5de159db184
m_aqsam <maqsam1155@gmail.com>
2024-09-06 23:41:04 +0500

    new changes

--- 07_Ecommerce_App/e_shop/lib/utlis/constants/colors.dart
+++ 07_Ecommerce_App/e_shop/lib/utlis/constants/colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//* All Colors constants are here
+
 class EColors {
   EColors._();
 
@@ -12,10 +14,32 @@ class EColors {
   static const Color textSecondary = Color(0xFF6C757D);
   static const Color textWhite = Colors.white;
 // Background Colors
-  static const Color Light = Color(0xFFF6F6F6);
+  static const Color light = Color(0xFFF6F6F6);
   static const Color dark = Color(0xFF272727);
   static const Color primaryBackground = Color(0xFFF3F5FF);
-  //
-  //
-  //
+
+  // Background Container Colors
+  static const Color lightContainer = Color(0xFFF6F6F6);
+  static Color darkContainer = EColors.white.withOpacity(0.1);
+// Button Colors
+  static const Color buttonPrimary = Color(0xFF4b68ff);
+  static const Color buttonSecondary = Color(0xFF6C757D);
+  static const Color buttonDisabled = Color(0xFFC4C4C4);
+
+  // Border Colors
+  static const Color borderPrimary = Color(0xFFD9D9D9);
+  static const Color borderSecondary = Color(0xFFE6E6E6);
+// Error and Validation Colors
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+  static const Color warning = Color(0xFFF57C00);
+  static const Color info = Color(0xFF1976D2);
+// Neutral Shades
+  static const Color black = Color(0xFF232323);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color white = Color(0xFFFFFFFF);
 }
