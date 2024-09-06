1798d6f5fd3415bc1b1d4c24bb0923dbc3b79d3f
m_aqsam <maqsam1155@gmail.com>
2024-09-06 23:40:50 +0500

    new changes

--- 07_Ecommerce_App/e_shop/lib/main.dart
+++ 07_Ecommerce_App/e_shop/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:e_shop/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  //WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  //FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const App());
 
   // Todo: Add Widget Binding
