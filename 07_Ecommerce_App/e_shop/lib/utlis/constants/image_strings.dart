7a98e81e99268644eafe57b1a8529e385df1191e
m_aqsam <maqsam1155@gmail.com>
2024-09-06 23:41:10 +0500

    new changes

---
+++ 07_Ecommerce_App/e_shop/lib/utlis/constants/image_strings.dart
@@ -0,0 +1,12 @@
+//* All Images used in app are here
+
+class EImages {
+  // App Logo
+  static String lightAppLogo = "";
+  static String darkAppLogo = "";
+
+  // Social Platforms
+
+  static String facebook = "";
+  static String google = "";
+}
