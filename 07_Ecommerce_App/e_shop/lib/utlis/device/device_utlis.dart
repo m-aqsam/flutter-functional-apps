780eba16804bca3f1dddc2f004372db4a63fb030
m_aqsam <maqsam1155@gmail.com>
2024-09-08 04:41:18 +0500

    update

--- 07_Ecommerce_App/e_shop/lib/utlis/device/device_utlis.dart
+++ 07_Ecommerce_App/e_shop/lib/utlis/device/device_utlis.dart
@@ -4,7 +4,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 class EDeviceUtlis {
