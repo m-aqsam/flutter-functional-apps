6f0bd42e58643d85aab8119d324c71ce6d384a3d
m_aqsam <maqsam1155@gmail.com>
2024-09-08 04:40:37 +0500

    new changes

---
+++ 07_Ecommerce_App/e_shop/lib/features/onboardingscreens/ui/components/on_boarding_page.dart
@@ -0,0 +1,48 @@
+import 'package:e_shop/utlis/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  final String image;
+  final String text;
+  final String subText;
+  const OnBoardingPage({
+    super.key,
+    required this.image,
+    required this.text,
+    required this.subText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          // Image/Illustration
+
+          Image(
+            height: EHelperFunctions.screenHeight() * 0.65,
+            width: EHelperFunctions.screenWidth() * 0.8,
+            image: AssetImage(image),
+          ),
+
+          // Heading
+
+          Text(
+            text,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+
+          SizedBox(height: 12),
+          // Sub Heading
+          Text(
+            subText,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
