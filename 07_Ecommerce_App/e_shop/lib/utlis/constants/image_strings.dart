385f0cfc08752bd6243337521f63958b74225ca2
m_aqsam <maqsam1155@gmail.com>
2024-09-08 04:41:04 +0500

    New images

--- 07_Ecommerce_App/e_shop/lib/utlis/constants/image_strings.dart
+++ 07_Ecommerce_App/e_shop/lib/utlis/constants/image_strings.dart
@@ -9,4 +9,13 @@ class EImages {
 
   static String facebook = "";
   static String google = "";
+
+  // OnBoarding Images
+
+  static String onBoardingImage1 =
+      "assets/images/on_boarding_images/select-product.gif";
+  static String onBoardingImage2 =
+      "assets/images/on_boarding_images/select-payment-method.gif";
+  static String onBoardingImage3 =
+      "assets/images/on_boarding_images/deliver-product.gif";
 }
