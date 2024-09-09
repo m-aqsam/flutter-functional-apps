0bb11302a9d0374e5861bdfca82f7583015cfc0f
m_aqsam <maqsam1155@gmail.com>
2024-09-09 22:53:19 +0500

    updated

---
+++ 07_Ecommerce_App/e_shop/lib/features/onboardingscreens/ui/components/skip_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SkipButton extends StatelessWidget {
+  final void Function()? onTAp;
+  const SkipButton({super.key, required this.onTAp});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: kToolbarHeight,
+      right: Checkbox.width,
+      child: TextButton(
+        onPressed: onTAp,
+        child: Text("Skip"),
+      ),
+    );
+  }
+}
