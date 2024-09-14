75f3a5a792d3b8317d434bd3537c98fa1130d1ae
m_aqsam <maqsam1155@gmail.com>
2024-09-14 06:41:24 +0500

    update

--- 07_Ecommerce_App/e_shop/lib/features/onboardingscreens/ui/screens/OnBoardingScreens.dart
+++ 07_Ecommerce_App/e_shop/lib/features/onboardingscreens/ui/screens/OnBoardingScreens.dart
@@ -77,8 +77,6 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
           //* Rounded Button
 
-          //
-
           Rounded_Button(onTap: () {
             if (currentIndex == 2) {
               // Go To Login PAge
