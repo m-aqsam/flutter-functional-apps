b7ea5826ed51010c6d2d03500f30cf1e06158f44
m_aqsam <maqsam1155@gmail.com>
2024-09-26 23:55:00 +0500

    New changes

--- 07_Ecommerce_App/e_shop/lib/utlis/constants/colors.dart
+++ 07_Ecommerce_App/e_shop/lib/utlis/constants/colors.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class EColors {
   EColors._();
 
-  // App Basic Colors
+// App Basic Colors
   static const Color primary = Color(0xFF4b68ff);
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
@@ -18,7 +18,7 @@ class EColors {
   static const Color dark = Color(0xFF272727);
   static const Color primaryBackground = Color(0xFFF3F5FF);
 
-  // Background Container Colors
+// Background Container Colors
   static const Color lightContainer = Color(0xFFF6F6F6);
   static Color darkContainer = EColors.white.withOpacity(0.1);
 // Button Colors
@@ -26,7 +26,7 @@ class EColors {
   static const Color buttonSecondary = Color(0xFF6C757D);
   static const Color buttonDisabled = Color(0xFFC4C4C4);
 
-  // Border Colors
+// Border Colors
   static const Color borderPrimary = Color(0xFFD9D9D9);
   static const Color borderSecondary = Color(0xFFE6E6E6);
 // Error and Validation Colors
