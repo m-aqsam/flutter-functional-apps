37b8676189f2f0dcb75618c6a0ae52698f36ceed
m_aqsam <maqsam1155@gmail.com>
2024-09-27 23:42:02 +0500

    new update

--- 07_Ecommerce_App/e_shop/lib/app.dart
+++ 07_Ecommerce_App/e_shop/lib/app.dart
@@ -8,7 +8,7 @@ import 'package:get/get.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application...
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
