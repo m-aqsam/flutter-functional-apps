41b845e01ed91d96fbe5991b39cd2c65e8382840
m_aqsam <maqsam1155@gmail.com>
2024-09-30 23:51:35 +0500

    new changes

--- 07_Ecommerce_App/e_shop/lib/utlis/theme/custom_themes/appbar_theme.dart
+++ 07_Ecommerce_App/e_shop/lib/utlis/theme/custom_themes/appbar_theme.dart
@@ -23,5 +23,5 @@ class EAppBarTheme {
     actionsIconTheme: IconThemeData(color: Colors.white, size: 24),
     titleTextStyle: TextStyle(
         fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.white),
-  ); // AppBarTheme
+  ); // AppBarTheme .
 }
