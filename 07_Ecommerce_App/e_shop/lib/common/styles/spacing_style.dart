1d716f9b2fabfb46462cf41ccb0c628e3e86a058
m_aqsam <maqsam1155@gmail.com>
2024-10-06 23:52:06 +0500

    new added

---
+++ 07_Ecommerce_App/e_shop/lib/common/styles/spacing_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SpacingStyle {
+
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: 56.0,
+    bottom: 24.0,
+    left: 24.0,
+    right: 24.0,
+  );
+}
