9a4fbed3968f6bb61aa0f810434125c76e89dd11
m_aqsam <maqsam1155@gmail.com>
2024-10-08 23:52:39 +0500

    form widget

---
+++ 07_Ecommerce_App/e_shop/lib/common/widgets/form_divider.dart
@@ -0,0 +1,41 @@
+import 'package:e_shop/utlis/constants/colors.dart';
+import 'package:e_shop/utlis/constants/text_strings.dart';
+import 'package:e_shop/utlis/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class Form_Divider extends StatelessWidget {
+  final String text;
+  const Form_Divider({
+    super.key,
+    required this.text,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = EHelperFunctions.isDarkMode(context);
+    return Row(
+      children: [
+        Flexible(
+          child: Divider(
+            color: dark ? EColors.grey : EColors.darkGrey,
+            thickness: 0.5,
+            endIndent: 8,
+            indent: 40,
+          ),
+        ),
+        Text(
+          text,
+          style: Theme.of(context).textTheme.labelMedium,
+        ),
+        Flexible(
+          child: Divider(
+            color: dark ? EColors.grey : EColors.darkGrey,
+            thickness: 0.5,
+            endIndent: 40,
+            indent: 8,
+          ),
+        ),
+      ],
+    );
+  }
+}
