cbd8e803b694892c7546bd7049ed862e2879d090
m_aqsam <maqsam1155@gmail.com>
2024-10-08 23:53:39 +0500

    update

--- 07_Ecommerce_App/e_shop/lib/features/login/bloc/bloc/login_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'login_bloc.dart';
-
-@immutable
-sealed class LoginState {}
-sealed class LoginActionState extends LoginState {}
-
-final class LoginInitial extends LoginState {}
-final class LoginLoading extends LoginState {}
