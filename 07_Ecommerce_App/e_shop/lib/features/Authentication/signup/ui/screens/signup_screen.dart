503d221fec671578f8cc00b25765fa4ccde378fa
m_aqsam <maqsam1155@gmail.com>
2024-10-08 23:53:18 +0500

    new update

---
+++ 07_Ecommerce_App/e_shop/lib/features/Authentication/signup/ui/screens/signup_screen.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class Signup_Screen extends StatelessWidget {
+  const Signup_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+            //
+          ],
+        ),
+      ),
+    );
+  }
+}
