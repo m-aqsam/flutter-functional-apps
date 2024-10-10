5c18b0b769b348666bbb05dcb4d884a45ad4f57b
m_aqsam <maqsam1155@gmail.com>
2024-10-10 23:48:53 +0500

    verify email screen

---
+++ 07_Ecommerce_App/e_shop/lib/features/Authentication/signup/ui/screens/verify_email.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:e_shop/common/styles/spacing_style.dart';
+import 'package:flutter/material.dart';
+
+class Verify_Email_Screen extends StatelessWidget {
+  const Verify_Email_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: ESpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              //***************  Image ***************
+
+              //***************  Tittle ***************
+
+              //***************  Description ***************
+
+              //***************  Buttons ***************
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
