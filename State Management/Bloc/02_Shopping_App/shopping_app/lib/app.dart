1752ed15912728ef4876f6851ef8385251825a9d
m_aqsam <maqsam1155@gmail.com>
2024-10-10 23:49:24 +0500

    update

---
+++ State Management/Bloc/02_Shopping_App/shopping_app/lib/app.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:shopping_app/features/Home/Ui/Home_Page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home_Page(),
+    );
+  }
+}
